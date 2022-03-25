06ddcb508f411b5188d5f19cdc784a6e25adcdec
Ngi Bujri <nbujri7@gmail.com>
2022-03-25 13:51:27 -0400

    added eventListeners for each button and displays for scores and results.

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
     const random = Math.floor((Math.random() * 3) + 1)
     switch (random) {
@@ -19,41 +22,53 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = this.value.toLowerCase();
     computerSelection = computerPlay().toLowerCase();
+    let message = "";
 
-    if (playerSelection === computerSelection) {
-        console.log(`You Chose: ${playerSelection} | Computer Chose: ${computerSelection}`);
-        return "It's A Tie!"
-    }else if (playerSelection === "rock" && computerSelection === "scissor") {
-        console.log(`You Chose: ${playerSelection} | Computer Chose: ${computerSelection}`);
-        return "You Win! Rock Beats Scissor."
-    }else if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log(`You Chose: ${playerSelection} | Computer Chose: ${computerSelection}`);
-        return "You Lose! Paper Beats Rock."
-    }else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log(`You Chose: ${playerSelection} | Computer Chose: ${computerSelection}`);
-        return "You Win! Paper Beats Rock."
-    }else if (playerSelection === "paper" && computerSelection === "scissor") {
-        console.log(`You Chose: ${playerSelection} | Computer Chose: ${computerSelection}`);
-        return "You Lose! Scissor Beats Paper."
-    }else if (playerSelection === "scissor" && computerSelection === "paper") {
-        console.log(`You Chose: ${playerSelection} | Computer Chose: ${computerSelection}`);
-        return "You Win! Scissor Beats Paper."
-    }else if (playerSelection === "scissor" && computerSelection === "rock") {
-        console.log(`You Chose: ${playerSelection} | Computer Chose: ${computerSelection}`);
-        return "You Lose! Rock Beats Scissor."
-    }else {
-        return "Game Cancelled."
+    if (playerScore < 5 && computerScore < 5) {
+        if (playerSelection === computerSelection) {
+            message += `You Chose: ${playerSelection} | Computer Chose: ${computerSelection}. \nIt's A Tie!`;
+        }else if (playerSelection === "rock" && computerSelection === "scissor") {
+            message += `You Chose: ${playerSelection} | Computer Chose: ${computerSelection}. \nYou Win! Rock Beats Scissor.`;
+            playerScore += 1;
+        }else if (playerSelection === "rock" && computerSelection === "paper") {
+            message += `You Chose: ${playerSelection} | Computer Chose: ${computerSelection}. \nYou Lose! Paper Beats Rock.`;
+            computerScore += 1;
+        }else if (playerSelection === "paper" && computerSelection === "rock") {
+            message += `You Chose: ${playerSelection} | Computer Chose: ${computerSelection}. \nYou Win! Paper Beats Rock.`;
+            playerScore += 1;
+        }else if (playerSelection === "paper" && computerSelection === "scissor") {
+            message += `You Chose: ${playerSelection} | Computer Chose: ${computerSelection}. \nYou Lose! Scissor Beats Paper.`;
+            computerScore += 1;
+        }else if (playerSelection === "scissor" && computerSelection === "paper") {
+            message += `You Chose: ${playerSelection} | Computer Chose: ${computerSelection}. \nYou Win! Scissor Beats Paper.`;
+            playerScore += 1;
+        }else if (playerSelection === "scissor" && computerSelection === "rock") {
+            message += `You Chose: ${playerSelection} | Computer Chose: ${computerSelection}. \nYou Lose! Rock Beats Scissor.`;
+            computerScore += 1;
+        }else {
+            message += "Game Cancelled."
+        }
+    } else {
+        if (playerScore === 5) {
+            message = `YOU HAVE 5 POINTS. YOU WIN!`
+        } else if (computerScore === 5) {
+            message = `COMPUTER HAS 5 POINTS. YOU LOSE!`
+        }
     }
-}
 
-function game() {
-    // for (let i = 0; i < 5; i++) {
-    //     console.log(playRound());
-    // }
+    
+
+    playerScoreDisplay.textContent = `PLAYER SCORE: ${playerScore}`;
+    computerScoreDisplay.textContent = `COMPUTER SCORE: ${computerScore}`;
+    displayResult.textContent = message;
 }
 
 const buttons = document.querySelectorAll('button');
+const displayResult = document.querySelector('.display-result')
+const playerScoreDisplay = document.querySelector('.player-score')
+const computerScoreDisplay = document.querySelector('.computer-score')
 buttons.forEach(button => button.addEventListener('click', playRound))
+
 
 
 
